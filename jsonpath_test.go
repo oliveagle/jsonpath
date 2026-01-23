f747ad68682916881ac6f329f8fc0f4994e81ee6
oliveagle <oliveagle@gmail.com>
2026-01-23 18:35:03 +0800

    refactor: extract comparison tests to jsonpath_comparison_test.go
    - Move Test_jsonpath_types_eval, Test_jsonpath_cmp_any, tcase_cmp_any to dedicated file
    - Remove unused go/token and go/types imports from jsonpath_test.go
    - No functional changes, pure refactoring

--- jsonpath_test.go
+++ jsonpath_test.go
@@ -9,8 +9,6 @@ package jsonpath
 import (
 	"encoding/json"
 	"fmt"
-	"go/token"
-	"go/types"
 	"regexp"
 	"testing"
 )
@@ -168,105 +166,6 @@ func Test_jsonpath_authors_of_all_books(t *testing.T) {
 	}
 	res, _ := JsonPathLookup(json_data, query)
 	t.Log(res, expected)
-}
-
-func Test_jsonpath_types_eval(t *testing.T) {
-	fset := token.NewFileSet()
-	res, err := types.Eval(fset, nil, 0, "1 < 2")
-	t.Logf("err: %v, res: %v, res.Type: %v, res.Value: %v, res.IsValue: %v", err, res, res.Type, res.Value, res.IsValue())
-}
-
-var (
-	ifc1 interface{} = "haha"
-	ifc2 interface{} = "ha ha"
-)
-var tcase_cmp_any = []map[string]interface{}{
-
-	map[string]interface{}{
-		"obj1": 1,
-		"obj2": 1,
-		"op":   "==",
-		"exp":  true,
-		"err":  nil,
-	},
-	map[string]interface{}{
-		"obj1": 1,
-		"obj2": 2,
-		"op":   "==",
-		"exp":  false,
-		"err":  nil,
-	},
-	map[string]interface{}{
-		"obj1": 1.1,
-		"obj2": 2.0,
-		"op":   "<",
-		"exp":  true,
-		"err":  nil,
-	},
-	map[string]interface{}{
-		"obj1": "1",
-		"obj2": "2.0",
-		"op":   "<",
-		"exp":  true,
-		"err":  nil,
-	},
-	map[string]interface{}{
-		"obj1": "1",
-		"obj2": "2.0",
-		"op":   ">",
-		"exp":  false,
-		"err":  nil,
-	},
-	map[string]interface{}{
-		"obj1": 1,
-		"obj2": 2,
-		"op":   "=~",
-		"exp":  false,
-		"err":  "op should only be <, <=, ==, >= and >",
-	}, {
-		"obj1": ifc1,
-		"obj2": ifc1,
-		"op":   "==",
-		"exp":  true,
-		"err":  nil,
-	}, {
-		"obj1": ifc2,
-		"obj2": ifc2,
-		"op":   "==",
-		"exp":  true,
-		"err":  nil,
-	}, {
-		"obj1": 20,
-		"obj2": "100",
-		"op":   ">",
-		"exp":  false,
-		"err":  nil,
-	},
-}
-
-func Test_jsonpath_cmp_any(t *testing.T) {
-	for idx, tcase := range tcase_cmp_any {
-		//for idx, tcase := range tcase_cmp_any[8:] {
-		t.Logf("idx: %v, %v %v %v, exp: %v", idx, tcase["obj1"], tcase["op"], tcase["obj2"], tcase["exp"])
-		res, err := cmp_any(tcase["obj1"], tcase["obj2"], tcase["op"].(string))
-		exp := tcase["exp"].(bool)
-		exp_err := tcase["err"]
-		if exp_err != nil {
-			if err == nil {
-				t.Errorf("idx: %d error not raised: %v(exp)", idx, exp_err)
-				break
-			}
-		} else {
-			if err != nil {
-				t.Errorf("idx: %v, error: %v", idx, err)
-				break
-			}
-		}
-		if res != exp {
-			t.Errorf("idx: %v, %v(got) != %v(exp)", idx, res, exp)
-			break
-		}
-	}
 }
 
 func Test_jsonpath_string_equal(t *testing.T) {
